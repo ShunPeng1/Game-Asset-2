9692c9193a0b0519083db3dcb3387000e9113f3d
ShunPeng1 <91878382+ShunPeng1@users.noreply.github.com>
2024-04-04 08:48:22 +0700

    feat: observable data

---
+++ Shun Collections/Shun Utilities/ObservableObject/ObservableData.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ShunCollection.Shun_Utilities.ObservableObject
+{
+    public interface IObservableData<T>
+    {
+        
+    }
+    
+    public class ObservableData<T> : IObservableData<T>
+    {
+        private T _value;
+
+        public T Value
+        {
+            get => _value;
+            set
+            {
+                if (_value.Equals(value))
+                {
+                    return;
+                }
+                var oldValue = _value;
+                _value = value;
+                OnValueChanged?.Invoke(oldValue, value);
+            }
+        }
+
+        public Action<T,T> OnValueChanged { get; protected set; } = delegate { };
+
+        public ObservableData(T value)
+        {
+            _value = value;
+        }
+    }
+}
